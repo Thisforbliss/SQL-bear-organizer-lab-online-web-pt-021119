d1afc406678ab2e0fb17af9c419db18adbe0570f
Malcolm Leake <echelon1089@gmail.com>
2019-04-29 23:31:50 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 17, "male", "brown", "cool", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 18, "male", "brown", "cool", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("craig", 9, "male", "brown", "cool", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 17, "male", "brown", "cool", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Seargeant Brown", 20, "male", "brown", "cool", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 21, "male", "brown", "cool", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 23, "male", "brown", "cool", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ( NULL , 50 , "male", "brown", "cool", 0);
--- lib/seed.sql
+++ lib/seed.sql
@@ -7,3 +7,27 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
+
+
+
+
+
+-- def highest_student_gpa
+--   "SELECT MAX(gpa) FROM students;"
+-- end
+
+-- def lowest_student_gpa
+--   "SELECT MIN(gpa) FROM students;"
+-- end
+
+-- def average_student_gpa
+--   "SELECT AVG(gpa) FROM students;"
+-- end
+
+-- def total_tardies_for_all_students
+--   "SELECT SUM(tardies) FROM students;"
+-- end
+
+-- def average_gpa_for_9th_grade
+--   "SELECT AVG(gpa) FROM students WHERE grade = 9;"
+-- end
